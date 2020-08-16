4458eae87d3136d50f2129e6cc067467e8108530
姜赟 <suukee@jiangyundeMacBook-Pro.local>
2020-08-16 18:14:05 +0800

    remove svg loader

--- vue.config.js
+++ vue.config.js
@@ -36,9 +36,4 @@ module.exports = {
       },
     },
   },
-  chainWebpack: config => {
-    const svgRule = config.module.rule('svg');
-    svgRule.uses.clear();
-    svgRule.use('vue-svg-loader').loader('vue-svg-loader');
-  }
 }
