327c39a56d7b77423c87abde03bd2229463673e7
姜赟 <suukee@jiangyundeMacBook-Pro.local>
2020-09-20 16:31:50 +0800

    fixbug

--- src/components/MainScheduleModal.vue
+++ src/components/MainScheduleModal.vue
@@ -15,22 +15,21 @@
       :labelCol="{ span: 6 }"
       :wrapperCol="{ span: 18 }"
     >
-      <a-form-model-item label="开始时间" prop="startDate">
-        <a-date-picker
-          v-model="form.startDate"
-          format="YYYY/MM/DD HH:mm"
-          show-time
+      <a-form-model-item label="开始时间" prop="startTime">
+        <a-time-picker
+          v-model="form.startTime"
+          format="HH:mm"
+          value-format="HH:mm"
           style="width: 240px;"
-        ></a-date-picker>
+        ></a-time-picker>
       </a-form-model-item>
-      <a-form-model-item label="结束时间" prop="endDate">
-        <a-date-picker
-          v-model="form.endDate"
-          format="YYYY/MM/DD HH:mm"
-          show-time
-          :disabledDate="(date) => date.isBefore($moment(form.startDate))"
+      <a-form-model-item label="结束时间" prop="endTime">
+        <a-time-picker
+          v-model="form.endTime"
+          format="HH:mm"
+          value-format="HH:mm"
           style="width: 240px;"
-        ></a-date-picker>
+        ></a-time-picker>
       </a-form-model-item>
       <a-form-model-item label="主日程名称" prop="name">
         <a-input
@@ -48,15 +47,15 @@ import { mapActions } from 'vuex';
 export default {
   name: 'MainScheduleModal',
   data() {
-    const endDateValidator = (rule, value, cb) => {
-      if (this.form.startDate) {
-        const isValid = this.$moment(value).isAfter(
-          this.$moment(this.form.startDate),
+    const endTimeValidator = (rule, value, cb) => {
+      if (this.form.startTime) {
+        const isValid = this.$moment(value, 'HH:mm').isAfter(
+          this.$moment(this.form.startTime, 'HH:mm'),
         );
         if (isValid) {
           cb();
         } else {
-          cb(new Error('结束时间不能遭遇开始时间'));
+          cb(new Error('结束时间不能早于开始时间'));
         }
       } else {
         cb();
@@ -67,17 +66,17 @@ export default {
       loading: false,
       recordId: null,
       form: {
-        startDate: null,
-        endDate: null,
+        startTime: null,
+        endTime: null,
         name: null,
       },
       rules: {
-        startDate: [
+        startTime: [
           { required: true, message: '开始时间不能为空', trigger: 'change' },
         ],
-        endDate: [
+        endTime: [
           { required: true, message: '结束时间不能为空', trigger: 'change' },
-          { validator: endDateValidator, trigger: 'change' },
+          { validator: endTimeValidator, trigger: 'change' },
         ],
         name: [
           { required: true, message: '主日程名称不能为空', trigger: 'blur' },
@@ -98,8 +97,8 @@ export default {
       this.visible = false;
       this.recordId = null;
       this.form = {
-        startDate: null,
-        endDate: null,
+        startTime: null,
+        endTime: null,
         name: null,
       };
       this.$refs.form.clearValidate();
