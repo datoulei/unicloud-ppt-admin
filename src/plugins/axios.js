9a87b5ef46627b03614eca11d5cf6fe1b2703bf6
Suukee <jiangyunisland@icloud.com>
2020-10-30 17:35:50 +0800

    添加请求超时时间

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-import Vue from "vue";
-import axios from "axios";
+import Vue from 'vue';
+import axios from 'axios';
 import _ from 'lodash';
-import { message } from 'ant-design-vue'
+import { message } from 'ant-design-vue';
 
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
@@ -11,8 +11,8 @@ import { message } from 'ant-design-vue'
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL: process.env.VUE_APP_BASE_URL || process.env.apiUrl || "",
-  timeout: 30 * 1000, // Timeout
+  baseURL: process.env.VUE_APP_BASE_URL || process.env.apiUrl || '',
+  timeout: 10 * 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
 
@@ -20,19 +20,19 @@ const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
   function (config) {
-    const loginType = Vue.db.get('loginType').value()
-    const token = Vue.db.get('token').value()
-    const baseURL = Vue.db.get('baseURL').value()
+    const loginType = Vue.db.get('loginType').value();
+    const token = Vue.db.get('token').value();
+    const baseURL = Vue.db.get('baseURL').value();
     if (loginType === 'internet') {
       if (config.baseURL !== process.env.VUE_APP_BASE_URL) {
-        config.baseURL = process.env.VUE_APP_BASE_URL
+        config.baseURL = process.env.VUE_APP_BASE_URL;
       }
       Object.assign(config.headers, {
-        'Authorization': `Bearer ${token}`
-      })
+        Authorization: `Bearer ${token}`,
+      });
     } else {
       if (config.baseURL !== baseURL) {
-        config.baseURL = baseURL
+        config.baseURL = baseURL;
       }
     }
     // Do something before request is sent
@@ -41,7 +41,7 @@ _axios.interceptors.request.use(
   function (error) {
     // Do something with request error
     return Promise.reject(error);
-  }
+  },
 );
 
 // Add a response interceptor
@@ -52,15 +52,15 @@ _axios.interceptors.response.use(
   },
   function (error) {
     if (error.response) {
-      const msg = _.get(error, 'response.data.message')
-      console.log("message", msg)
-      message.error(msg)
+      const msg = _.get(error, 'response.data.message');
+      console.log('message', msg);
+      message.error(msg);
     } else {
-      message.error('网络异常，请检查')
+      message.error('网络异常，请检查');
     }
     // Do something with response error
     return Promise.reject(error);
-  }
+  },
 );
 
 Plugin.install = function (Vue) {
@@ -70,13 +70,13 @@ Plugin.install = function (Vue) {
     axios: {
       get() {
         return _axios;
-      }
+      },
     },
     $axios: {
       get() {
         return _axios;
-      }
-    }
+      },
+    },
   });
 };
 
